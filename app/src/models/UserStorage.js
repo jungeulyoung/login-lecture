51617c001dcaf6834aa753e67b58400b7b06f2a3
eulyoungJung <@eulyoungjung09>
2022-07-27 15:28:43 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,8 +17,9 @@ class UserStorage {
     }
 
 
-    static getUsers(...fields) {
-        // const users = this.#users;
+    static #getUsers(data, isAll, fields) {
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers, field)=> {
             if(users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -26,6 +27,15 @@ class UserStorage {
             return newUsers;
         },{});
         return newUsers;
+    }
+
+    static getUsers(isAll, ...fields) {
+        return fs
+            .readFile("./src/databases/users.json")
+            .then((data)=> {
+                return this.#getUsers(data, isAll, fields);
+            })
+            .catch(console.error); 
     }
 
 
@@ -38,14 +48,17 @@ class UserStorage {
             .catch(console.error); 
     }
 
-   
 
-    static save(userInfo) {
-        // const users = this.#users;
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)){
+            throw "이미 존재하는 아이디 입니다.";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return { success: true};
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return { success: true };
     }
 }
 
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,9 +1,9 @@
 "use strict";
+
 const express = require('express');
 const router = express.Router();
 
 const ctrl = require("./home.ctrl")
-
 
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
