b926825997c58f3b4d22f0ead45abd5f1c2b998c
eulyoungJung <@eulyoungjung09>
2022-07-28 17:43:41 +0900

    winston 프로젝트에 적용

--- app/app.js
+++ app/app.js
@@ -4,12 +4,10 @@
 const express = require("express");
 const bodyParser = require ("body-parser");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
-
-const app = express();
 dotenv.config();
 
-const accessLogStream = require("./src/config/log");
+
+const app = express();
 
 //라우팅
 const home = require("./src/routes/home");
@@ -23,8 +21,8 @@ app.use(bodyParser.json());
 
 // URL 을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use(morgan("common", { stream: accessLogStream }) );
+// app.use(morgan("tiny", { stream: logger.stream }));
+
 
 app.use("/", home); // use --> 미들 웨어를 등록해주는 메서드.
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -11,15 +11,14 @@ registerBtn.addEventListener("click", register);
 
     function register() {
         if(!id.value) return alert('아이디를 입력해주세요.');
-        if(psword.value !== confirmPsword.value) return alert('비밀번호가 일치하지 않습니다.')
-        
-
+        if(psword.value !== confirmPsword.value) 
+            return alert('비밀번호가 일치하지 않습니다.');
+    
         const req = {
             id: id.value,
             name: name.value,
             psword: psword.value,
         };
-
 
         fetch("/register", {
             method: "POST",
@@ -33,6 +32,7 @@ registerBtn.addEventListener("click", register);
             if(res.success) {
                 location.href = "/login";
             }else {
+                if(res.err) return alert(res.err);
                 alert(res.msg);
             }
         })
